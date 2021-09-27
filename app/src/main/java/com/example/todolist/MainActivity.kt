c15289892ee8a458df43e9946fa5ab9e9c1292f8
Edherrera <edherrera96@uan.edu.co>
2021-09-26 19:58:22 -0500

    se agrego el boton eliminar tarea

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -6,7 +6,7 @@ import android.widget.*
 
 class MainActivity : AppCompatActivity() {
 
-    val simpsonNames = arrayListOf<String>("") // 1. crear arreglo de datos
+    val simpsonNames = arrayListOf<String>("Task 1") // 1. crear arreglo de datos
     var adapter: ArrayAdapter<String>?= null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,9 +15,9 @@ class MainActivity : AppCompatActivity() {
 
 
         adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, simpsonNames) // 2. crear un adapter usando el arreglo de datos
-        val simpsonsListView = findViewById<ListView>(R.id.simpsonsList)
-        simpsonsListView.adapter = adapter // 3. conectar el adapter con el listview
-        simpsonsListView.setOnItemClickListener { listview, listitem, index, id ->
+        val TaskListView = findViewById<ListView>(R.id.taskList)
+        TaskListView.adapter = adapter // 3. conectar el adapter con el listview
+        TaskListView.setOnItemClickListener { listview, listitem, index, id ->
             println("item: $listitem, index: $index, id: $id")
             val textview = listitem as TextView
             Toast.makeText(this, "Seleccionaste  ${textview.text}", Toast.LENGTH_SHORT).show()
@@ -25,28 +25,34 @@ class MainActivity : AppCompatActivity() {
 
         val miBoton = findViewById<Button>(R.id.btnAgregar)
         miBoton.setOnClickListener {
-            agregarPersonaje()
+            agregarTask()
         }
 
-
+        val miBoton2 = findViewById<Button>(R.id.deletebut)
+        miBoton2.setOnClickListener {
+            quitarPersonaje()
+        }
 
     }
 
-    fun agregarPersonaje() {
+    fun agregarTask() {
         val edAgregar: EditText = findViewById(R.id.edAgregar)
         val myTextView: TextView = findViewById(R.id.textView2)
         val nombre = edAgregar.text.toString()
 
         if (edAgregar.text.toString().length == 0) {
-            myTextView.text = "Campo vacio... ingrese un nuevo items"
+            Toast.makeText(this, "Campo Vacio... ingrese una task!!", Toast.LENGTH_SHORT).show()
         }
-
 
         simpsonNames.add(nombre)
         adapter?.notifyDataSetChanged()
+
     }
 
-
+    fun quitarPersonaje() {
+        simpsonNames.removeAt(simpsonNames.size-1)
+        adapter?.notifyDataSetChanged()
+    }
 
 
 
